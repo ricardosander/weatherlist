54b42543484a5c3def9af694b3dbb385f0494c1c
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-08 20:36:51 -0300

    Creating recommendation service (interface) and using it on controller.

--- src/main/java/br/com/ricardosander/weatherlist/controllers/RecommendationController.java
+++ src/main/java/br/com/ricardosander/weatherlist/controllers/RecommendationController.java
@@ -1,6 +1,9 @@
 package br.com.ricardosander.weatherlist.controllers;
 
 import br.com.ricardosander.weatherlist.dto.GeographicCoordinate;
+import br.com.ricardosander.weatherlist.entities.Playlist;
+import br.com.ricardosander.weatherlist.services.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/recommendation")
 public class RecommendationController {
 
+  private final RecommendationService recommendationService;
+
+  @Autowired
+  public RecommendationController(
+      RecommendationService recommendationService) {
+    this.recommendationService = recommendationService;
+  }
+
   @RequestMapping(method = RequestMethod.GET, value = "/city/{cityName}")
-  public ResponseEntity<String> getPlaylistByCityName(@PathVariable String cityName) {
-    return ResponseEntity.ok().body("City name : " + cityName);
+  public ResponseEntity<Playlist> getPlaylistByCityName(@PathVariable String cityName) {
+    return ResponseEntity.ok().body(recommendationService.getPlaylist(cityName));
   }
 
   @RequestMapping(method = RequestMethod.GET, value = "/coordinates")
-  public ResponseEntity<String> getPlaylistByGeographicCoordinates(@RequestParam double latitude,
+  public ResponseEntity<Playlist> getPlaylistByGeographicCoordinates(@RequestParam double latitude,
       @RequestParam double longitude) {
 
     GeographicCoordinate geCoordinate = GeographicCoordinate.newInstance(latitude, longitude);
 
-    return ResponseEntity.ok()
-        .body("latitude : " + geCoordinate.getLatitude() + ", longitude: "
-            + geCoordinate.getLongitude());
+    return ResponseEntity.ok().body(recommendationService.getPlaylist(geCoordinate));
   }
 
 }
---
+++ src/main/java/br/com/ricardosander/weatherlist/services/RecommendationService.java
@@ -0,0 +1,12 @@
+package br.com.ricardosander.weatherlist.services;
+
+import br.com.ricardosander.weatherlist.dto.GeographicCoordinate;
+import br.com.ricardosander.weatherlist.entities.Playlist;
+
+public interface RecommendationService {
+
+  Playlist getPlaylist(GeographicCoordinate geographicCoordinate);
+
+  Playlist getPlaylist(String cityName);
+
+}
