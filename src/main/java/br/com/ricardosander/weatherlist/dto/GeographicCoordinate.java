fe2affff88ce0747b62aabbf1b846a7235236e00
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-08 20:03:21 -0300

    Adding validation on creating instance to GeographicCoordinate class.

--- src/main/java/br/com/ricardosander/weatherlist/dto/GeographicCoordinate.java
+++ src/main/java/br/com/ricardosander/weatherlist/dto/GeographicCoordinate.java
@@ -1,6 +1,25 @@
 package br.com.ricardosander.weatherlist.dto;
 
+import static br.com.ricardosander.weatherlist.dto.GeographicCoordinateValidator.MAXIMUM_LATITUDE;
+import static br.com.ricardosander.weatherlist.dto.GeographicCoordinateValidator.MAXIMUM_LONGITUDE;
+import static br.com.ricardosander.weatherlist.dto.GeographicCoordinateValidator.MINIMUM_LATITUDE;
+import static br.com.ricardosander.weatherlist.dto.GeographicCoordinateValidator.MINIMUM_LONGITUDE;
+
+import java.security.InvalidParameterException;
+
 public class GeographicCoordinate {
+
+  private final static GeographicCoordinateValidator validator =
+      new GeographicCoordinateValidator();
+
+  private static final String INVALID_LATITUDE_MESSAGE =
+      "Latitude should be from " + MINIMUM_LATITUDE + " to " + MAXIMUM_LATITUDE;
+
+  private static final String INVALID_LONGITUDE_MESSAGE =
+      "Longitude should be from " + MINIMUM_LONGITUDE + " to " + MAXIMUM_LONGITUDE;
+
+  private static final String INVALID_GEOGRAPHIC_COORDINATES =
+      INVALID_LATITUDE_MESSAGE + " and " + INVALID_LONGITUDE_MESSAGE;
 
   private final double latitude;
 
@@ -12,7 +31,22 @@ public class GeographicCoordinate {
   }
 
   public static GeographicCoordinate newInstance(double latitude, double longitude) {
-    return new GeographicCoordinate(latitude, longitude);
+
+    GeographicCoordinate geoCoordinate = new GeographicCoordinate(latitude, longitude);
+
+    if (validator.isLatitudeValid(geoCoordinate) && validator.isLongitudeValid(geoCoordinate)) {
+      return geoCoordinate;
+    }
+
+    if (validator.isLatitudeValid(geoCoordinate)) {
+      throw new InvalidParameterException(INVALID_LONGITUDE_MESSAGE);
+    }
+
+    if (validator.isLongitudeValid(geoCoordinate)) {
+      throw new InvalidParameterException(INVALID_LATITUDE_MESSAGE);
+    }
+
+    throw new InvalidParameterException(INVALID_GEOGRAPHIC_COORDINATES);
   }
 
   public double getLatitude() {
--- src/test/java/br/com/ricardosander/weatherlist/dto/GeographicCoordinateTest.java
+++ src/test/java/br/com/ricardosander/weatherlist/dto/GeographicCoordinateTest.java
@@ -5,12 +5,14 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
+import java.security.InvalidParameterException;
+
 public class GeographicCoordinateTest {
 
   @Test
   public void newInstanceWithPositiveNumber() {
 
-    double latitude = 90.01;
+    double latitude = 90.00;
     double longitude = 80.29;
 
     GeographicCoordinate geoCoordinate = GeographicCoordinate.newInstance(latitude, longitude);
@@ -57,6 +59,42 @@ public class GeographicCoordinateTest {
     assertNotNull(geoCoordinate);
     assertEquals(latitude, geoCoordinate.getLatitude(), 0.1);
     assertEquals(longitude, geoCoordinate.getLongitude(), 0.1);
+  }
+
+  @Test(expected = InvalidParameterException.class)
+  public void invalidInstanceSinceLatitudeIstTooHigh() {
+
+    double latitude = 90.01;
+    double longitude = 80.29;
+
+    GeographicCoordinate.newInstance(latitude, longitude);
+  }
+
+  @Test(expected = InvalidParameterException.class)
+  public void invalidInstanceSinceLatitudeIsTooLow() {
+
+    double latitude = -90.01;
+    double longitude = 80.29;
+
+    GeographicCoordinate.newInstance(latitude, longitude);
+  }
+
+  @Test(expected = InvalidParameterException.class)
+  public void invalidInstanceSinceLongitudeIstTooHigh() {
+
+    double latitude = 80.0;
+    double longitude = 180.01;
+
+    GeographicCoordinate.newInstance(latitude, longitude);
+  }
+
+  @Test(expected = InvalidParameterException.class)
+  public void invalidInstanceSinceLongitudeIsTooLow() {
+
+    double latitude = 80.00;
+    double longitude = -180.01;
+
+    GeographicCoordinate.newInstance(latitude, longitude);
   }
 
 }
