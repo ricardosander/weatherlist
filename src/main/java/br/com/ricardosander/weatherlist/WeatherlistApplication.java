d7bac072852ff6cfcbc272c3a537fac6de194029
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-08 18:50:26 -0300

    Code formatting.

--- src/main/java/br/com/ricardosander/weatherlist/WeatherlistApplication.java
+++ src/main/java/br/com/ricardosander/weatherlist/WeatherlistApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherlistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherlistApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherlistApplication.class, args);
+  }
 }
