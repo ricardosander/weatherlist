8dd21f4bcd5ee5f80e18044680fcc7e848906ed3
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-07 21:03:31 -0300

    Adding Spring Boot by Sprint Boot Spring Initializr

---
+++ src/main/java/br/com/ricardosander/weatherlist/WeatherlistApplication.java
@@ -0,0 +1,12 @@
+package br.com.ricardosander.weatherlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherlistApplication.class, args);
+	}
+}
