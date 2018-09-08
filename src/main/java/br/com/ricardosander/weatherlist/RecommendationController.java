a7d7fe6d5fd8ea669f97f0a7e148029e85bd6026
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-08 17:52:07 -0300

    Creating basic controller to test application running.

---
+++ src/main/java/br/com/ricardosander/weatherlist/RecommendationController.java
@@ -0,0 +1,25 @@
+package br.com.ricardosander.weatherlist;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/recommendation")
+public class RecommendationController {
+
+  @RequestMapping(method = RequestMethod.GET, value = "/city/{cityName}")
+  public ResponseEntity<String> getPlaylistByCityName(@PathVariable String cityName) {
+    return ResponseEntity.ok().body("City name : " + cityName);
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/coordinates")
+  public ResponseEntity<String> getPlaylistByGeographicCoordinates(@RequestParam String latitude,
+      @RequestParam String longitude) {
+    return ResponseEntity.ok().body("latitude : " +latitude + ", longitude: " + longitude);
+  }
+
+}
