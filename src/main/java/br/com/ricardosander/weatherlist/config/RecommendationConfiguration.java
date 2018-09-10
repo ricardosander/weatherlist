f103018b61d31e96bb6378f3b60abc1d1a678aec
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-10 01:54:54 -0300

    Adding Spotify configuration to configuration class.

--- src/main/java/br/com/ricardosander/weatherlist/config/RecommendationConfiguration.java
+++ src/main/java/br/com/ricardosander/weatherlist/config/RecommendationConfiguration.java
@@ -7,11 +7,24 @@ import br.com.ricardosander.weatherlist.apis.WeatherAPI;
 import br.com.ricardosander.weatherlist.services.RecommendationService;
 import br.com.ricardosander.weatherlist.services.RecommendationServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class RecommendationConfiguration {
+
+  private final String spotifyClientId;
+
+  private final String spotifySecret;
+
+  public RecommendationConfiguration(@Value("${spotify.clientId}") String spotifyClientId,
+      @Value("${spotify.secret}") String spotifySecret) {
+    this.spotifyClientId = spotifyClientId;
+    this.spotifySecret = spotifySecret;
+  }
 
   @Bean
   @Autowired
@@ -27,7 +40,7 @@ public class RecommendationConfiguration {
 
   @Bean
   public PlaylistAPI playlistAPI() {
-    return new SpotifyAPI();
+    return new SpotifyAPI(spotifyClientId, spotifySecret);
   }
 
 }
