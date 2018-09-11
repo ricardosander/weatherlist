5e025a9db921aebc9192f5ef2c343bf8dac8d936
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-10 22:18:02 -0300

    Adding exception to interface.

--- src/main/java/br/com/ricardosander/weatherlist/apis/PlaylistAPI.java
+++ src/main/java/br/com/ricardosander/weatherlist/apis/PlaylistAPI.java
@@ -1,8 +1,9 @@
 package br.com.ricardosander.weatherlist.apis;
 
+import br.com.ricardosander.weatherlist.apis.exceptions.ApiUnavailableException;
 import br.com.ricardosander.weatherlist.entities.Category;
 import br.com.ricardosander.weatherlist.entities.Playlist;
 
 public interface PlaylistAPI {
-  Playlist find(Category category);
+  Playlist find(Category category) throws ApiUnavailableException;
 }
