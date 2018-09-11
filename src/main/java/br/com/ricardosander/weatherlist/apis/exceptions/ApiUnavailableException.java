b13544395f443463137d16073dc5fd72a818fbe1
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-10 22:11:48 -0300

    Creating api exceptions.

---
+++ src/main/java/br/com/ricardosander/weatherlist/apis/exceptions/ApiUnavailableException.java
@@ -0,0 +1,9 @@
+package br.com.ricardosander.weatherlist.apis.exceptions;
+
+public class ApiUnavailableException extends Exception {
+
+  public ApiUnavailableException(String message) {
+    super(message);
+  }
+
+}
---
+++ src/main/java/br/com/ricardosander/weatherlist/apis/exceptions/SpotifyApiUnavailableException.java
@@ -0,0 +1,9 @@
+package br.com.ricardosander.weatherlist.apis.exceptions;
+
+public class SpotifyApiUnavailableException extends ApiUnavailableException {
+
+  public SpotifyApiUnavailableException(String message) {
+    super(message);
+  }
+
+}
