43820d9628adb88d6a26e90b3b25ce83cef97486
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-09-10 22:25:46 -0300

    Handling API exception by returning a default playlist.

--- src/main/java/br/com/ricardosander/weatherlist/services/RecommendationServiceImplementation.java
+++ src/main/java/br/com/ricardosander/weatherlist/services/RecommendationServiceImplementation.java
@@ -2,12 +2,16 @@ package br.com.ricardosander.weatherlist.services;
 
 import br.com.ricardosander.weatherlist.apis.PlaylistAPI;
 import br.com.ricardosander.weatherlist.apis.WeatherAPI;
+import br.com.ricardosander.weatherlist.apis.exceptions.ApiUnavailableException;
 import br.com.ricardosander.weatherlist.dto.GeographicCoordinate;
 import br.com.ricardosander.weatherlist.entities.Category;
 import br.com.ricardosander.weatherlist.entities.Playlist;
+import br.com.ricardosander.weatherlist.entities.Track;
 import br.com.ricardosander.weatherlist.entities.Weather;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class RecommendationServiceImplementation implements RecommendationService {
@@ -16,10 +20,13 @@ public class RecommendationServiceImplementation implements RecommendationServic
 
   private final PlaylistAPI playlistAPI;
 
+  private final Playlist defaultPlaylist;
+
   @Autowired
   public RecommendationServiceImplementation(WeatherAPI weatherAPI, PlaylistAPI playlistAPI) {
     this.weatherAPI = weatherAPI;
     this.playlistAPI = playlistAPI;
+    defaultPlaylist = new Playlist(Arrays.asList(new Track("A Track"), new Track("Another Track")));
   }
 
   @Override
@@ -28,7 +35,11 @@ public class RecommendationServiceImplementation implements RecommendationServic
     Weather weather = weatherAPI.findWeather(geographicCoordinate);
     Category category = Category.getInstance(weather);
 
-    return playlistAPI.find(category);
+    try {
+      return playlistAPI.find(category);
+    } catch (ApiUnavailableException e) {
+      return defaultPlaylist;
+    }
   }
 
   @Override
@@ -37,7 +48,11 @@ public class RecommendationServiceImplementation implements RecommendationServic
     Weather weather = weatherAPI.findWeatherByCityName(cityName);
     Category category = Category.getInstance(weather);
 
-    return playlistAPI.find(category);
+    try {
+      return playlistAPI.find(category);
+    } catch (ApiUnavailableException e) {
+      return defaultPlaylist;
+    }
   }
 
 }
